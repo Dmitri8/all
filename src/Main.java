3567272e43a8ba01513eb98ae0c5b4cfc216ddb1
Dmitri8 <88377967+Dmitri8@users.noreply.github.com>
2021-08-03 15:23:33 +0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String...args) throws InterruptedException {
+    int [] arr = {12, 23, 12,  1, 4, 45, 53, 31, 46, 87, 976, 54, 34, 45};
+
+    for(int i = 2; i < arr.length; i++){
+
+    }
+
+
+    for(Integer i : arr) System.out.println(i);
+
+
+
+
+    }
+}
---
+++ src/Q.java
@@ -0,0 +1,8 @@
+
+public class Q implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
